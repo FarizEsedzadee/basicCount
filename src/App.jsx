a9d65859cca11637ad8df7cc35745281efecc75c
FarizEsedzadee <fariz.esedov.99@gmail.com>
2025-09-24 08:43:50 +0400

    - add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let [count, setCount] = useState(0)
-
+  let [count, setCount] = useState(0);
 
   return (
-    <>
-      <button onClick={() => setCount(count = (count <= 0) ? 0 : count - 1)}> - </button>
-      <span> {count} </span>
-      <button onClick={() => setCount(count = (count == 10) ? 10 : count + 1)}> + </button>
-    </>
-  )
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex items-center gap-4">
+        <button
+          onClick={() => setCount(count <= 0 ? 0 : count - 1)}
+          className="px-4 py-2 border text-xl"
+        >
+          -
+        </button>
+
+        <span className="text-2xl min-w-[40px] text-center">{count}</span>
+
+        <button
+          onClick={() => setCount(count >= 10 ? 10 : count + 1)}
+          className="px-4 py-2 border text-xl"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
